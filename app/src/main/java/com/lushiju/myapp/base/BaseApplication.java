5de03551f7f7a963366925ba24cae1ab30c2130a
Lushiju <1243951807@qq.com>
2017-04-17 16:12:22 +0800

    集成环信

--- app/src/main/java/com/lushiju/myapp/base/BaseApplication.java
+++ app/src/main/java/com/lushiju/myapp/base/BaseApplication.java
@@ -2,6 +2,8 @@ package com.lushiju.myapp.base;
 
 import android.app.Application;
 
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
 import com.zhy.autolayout.config.AutoLayoutConifg;
 
 /**
@@ -17,6 +19,17 @@ public class BaseApplication extends Application {
         super.onCreate();
         ins = this;
         AutoLayoutConifg.getInstance().useDeviceSize();
+        init();
+    }
+
+    private void init() {
+        EMOptions options = new EMOptions();
+        // 默认添加好友时，是不需要验证的，改成需要验证
+        options.setAcceptInvitationAlways(false);
+        //初始化
+        EMClient.getInstance().init(this, options);
+        //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
+        EMClient.getInstance().setDebugMode(true);
     }
 
     public static BaseApplication getInstance(){
